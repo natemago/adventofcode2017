ea952bbfb44ed0524dcd2f596134dc7a780aed65
Pavle Jonoski <jonoski.pavle@gmail.com>
2017-12-20 07:03:44 +0100

    goodenough

---
+++ day20_particle_swarm/solution.py
@@ -0,0 +1,41 @@
+﻿import re
+
+mn = None
+c=0
+with open('input') as f:
+	for ln in f:
+		p=ln.strip().split('>, ')
+		a=[int(i.strip()) for i in p[2].strip()[3:-1].split(',')]
+
+				
+
+#				print(a)
+		#if mn is None:
+		s=sum([abs(i) for i in a]) 
+		if mn is None:
+			mn=(s,a,ln,c)
+		if mn[0] >= s:
+			mn=(s,a,ln,c)
+		c+=1
+print(mn)
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
