62c94536f27e5307050436de8c22308adb3c208c
Pavle Jonoski <jonoski.pavle@gmail.com>
2017-12-23 18:27:00 +0100

    Day 20, solved both parts.

--- day20_particle_swarm/solution.py
+++ day20_particle_swarm/solution.py
@@ -73,32 +73,167 @@ re.findall('(p|a|v)=<([\\d,]+)>','p=<1,2,3>, v=<12,34,56>, a=<123,3>')
 
 
 import re
+import math
 
 
 class Particle:
-  def __init__(self, n, p, v, a):
-    self.n = n
-    self.p = p
-    self.a = a
-    self.v = v
+    def __init__(self, n, p=None, v=None, a=None):
+        self.n = n
+        self.p = p
+        self.a = a
+        self.v = v
+        self.collisions = []
+    
+    def __repr__(self):
+        return 'P(%d)'%self.n
+    
+    def __str__(self):
+        return self.__repr__()
+    
+    def p_at(self, n):
+        def cx(i, n):
+            return (n*(n+1)/2)*self.a[i] + n*self.v[i] + self.p[i]
+        return [cx(0, n), cx(1,n), cx(2,n) ]
+        
+    def collides(self, op):
+        coll_times = []
+        if self.p == op.p:
+            return [0]
+        for ax in range(0,3):
+            coll_times.append([])
+            X = self.p[ax] - op.p[ax]
+            V = self.v[ax] - op.v[ax]
+            A = (self.a[ax] - op.a[ax])/2
+            
+            if A == 0:
+                if V == 0:
+                    if X == 0:
+                        # special case, the particles are at the same spot - collide at every tick on this axis
+                        coll_times[ax].append(-1)
+                        continue
+                    else:
+                        return None 
+                n = -X/V
+                if n > 0:
+                    coll_times[ax].append(n)
+                    continue
+                return None
+                
+            if (V+A)**2 - 4*A*X < 0:
+                return None
+            n1 = solve(1, A, (V+A), X)
+            n2 = solve(-1, A, (V+A), X)
+            #print(n1,n2)
+            
+            if n1 > 0:
+                coll_times[ax].append(n1)
+            if n2 > 0:
+                coll_times[ax].append(n2)
+            if len(coll_times[ax]) == 0:
+                return None
+        #print(coll_times)
+        return collision_times(coll_times)
 
 
-mn = None
-c=0
-with open('input') as f:
-  for ln in f:
-     specs = re.findall('(p|a|v)=<([\\d,]+)>', line.strip())
-     print(specs)
-     
-     p=ln.strip().split('>, ')
-     a=[int(i.strip()) for i in p[2].strip()[3:-1].split(',')]
-     s=sum([abs(i) for i in a]) 
-     if mn is None:
-       mn=(s,a,ln,c)
-     if mn[0] >= s:
-       mn=(s,a,ln,c)
-     c+=1
-print(mn)
+def collision_times(ct):
+    Sn = set()
+    
+    for i in range(0,3): # axis
+        for v in ct[i]: # value
+            if (v in ct[(i+1)%3] or -1 in ct[(i+1)%3]) and (v in ct[(i+2)%3] or -1 in ct[(i+2)%3]):
+                Sn.add(v)
+    if len(Sn) == 0:
+        return None
+    return list(Sn)
+
+
+def solve(i, a, b, c):
+   return (-b +i*math.sqrt(b**2-4*a*c) )/(2*a)               
+
+def load_input(f):
+    c=0
+    particles = []
+    with open(f) as inpf:
+        for ln in inpf:
+            specs = re.findall('(p|a|v)=<([-\\d,]+)>', ln.strip())
+            #print(specs)
+            particle = Particle(n=c)
+            for s in specs:
+                vals = [int(i.strip()) for i in s[1].split(',')]
+                
+                if s[0] == 'p':
+                    particle.p = vals
+                elif s[0] == 'v':
+                    particle.v = vals
+                else:
+                    particle.a = vals
+            
+            particles.append(particle)
+            c+=1
+    return particles
+
+def part2(particles):
+    for i in range(0, len(particles)-1):
+        for j in range(i+1, len(particles)):
+            p1 = particles[i]
+            p2 = particles[j]
+            Sn = p1.collides(p2)
+            if Sn:
+                #print(p1,'with',p2,'at',Sn)
+                for s in Sn:
+                    
+                    if (len(p1.collisions)==0 or s <= p1.collisions[0][0]) and (len(p2.collisions)==0 or s <= p2.collisions[0][0]):
+                        # this collision must be valid for both particles, but we must prune the other collisions as well
+                        #s = int(s)
+                        if len(p1.collisions):
+                            prune_collisions(p1, s)
+                        if len(p2.collisions):
+                            prune_collisions(p2, s)
+                            
+                        p1.collisions.append((s, p2))
+                        p2.collisions.append((s, p1))
+                    
+                        p1.collisions = [i for i in sorted(p1.collisions, key=lambda x: x[0])]
+                        p2.collisions = [i for i in sorted(p2.collisions, key=lambda x: x[0])]
+   
+    count = 0
+    for p in particles:
+        #print(p, p.collisions)
+        if len(p.collisions):
+            count+=1
+    return count, len(particles), len(particles) - count            
+
+def prune_collisions(p, col):
+    prune = []
+    for c in p.collisions:
+        if c[0] > col:
+            prune.append(c)
+    
+    for pr in prune:
+        p.collisions.remove(pr)
+        opr = []
+        for c in pr[1].collisions:
+            if c[1] == p:
+                opr.append(c)
+        
+        for c in opr:
+            pr[1].collisions.remove(c)    
+
+import sys
+
+particles = load_input('test_inp' if 'test' in sys.argv else 'input')
+print(len(particles),'particles')
+print('Part 1:', sorted( sorted(particles, key=lambda p: [abs(i) for i in p.p]), key=lambda p: [abs(i) for i in p.a])[0])
+print('Part 2: %d collisions of %d total particles. %d particles left after all colisions resolved.' % part2(particles))
+#print(particles[138].collides(particles[54]))
+
+#for i in range(0, 5):
+#    print('Tick: ', i)
+#    for p in particles:
+#        print(p, p.p_at(i))
+#    print('--------')
+
+
         
         
         
